73d79b42e65cb2e78fe28a7715dc49b6bf531ac0
rijonshahariar <shahariar.rijon@gmail.com>
2022-05-24 12:06:06 +0600

    normal initialization completed

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1pbhl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('arctools').collection('tools');
+
+        // AUTH
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '2d'
+            });
+            res.send({ accessToken });
+        })
+
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const results = await cursor.toArray();
+            res.send(results);
+        });
+
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Server Running');
+});
+
+app.listen(port, () => {
+    console.log(port);
+})
